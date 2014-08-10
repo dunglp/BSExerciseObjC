110b10488cb1588e8cc2d945a1359e28fbb2a836
Dzung Le <dunglp1303@gmail.com>
2014-08-10 23:20:29 +0700

    Initial Commit

---
+++ BSExerciseObjC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BSExerciseObjC
+//
+//  Created by Bi on 8/10/14.
+//  Copyright (c) 2014 BiStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
